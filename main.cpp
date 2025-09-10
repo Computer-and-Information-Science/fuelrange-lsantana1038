843a79b7657913f630b65e604098c5155e2d760b
lsantana1038 <lsantana1038@gmail.com>
2025-09-10 20:29:07 +0000

    Fuel Range

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 // TODO: YOUR NAME HERE
 #include <iostream>
 // TODO: Add the "using" statement
-
-int main() {
+  using namespace std;
+  int main() {
   // TODO: Declare your variables, with comments for each
-
+  // capacity of the fuel tank in gallons
+  double capacity;
+    //miles per gallon
+  double mpg;
+  //percentage filled
+  int percentFull;
+  
+  
   // TODO: Prompt for and input the required values
-
+  cout << "Please input the capacity of your fuel tank ";
+  cin >> capacity;
+  cout << "Please input the mpg of your car ";
+  cin >> mpg;
+  cout << "please enter the percent full: ";
+  cin >> percentFull;
   // TODO: Calculate the results
 
-  // TODO: Output the results
-  
+double remainingFuel = (percentFull*capacity)/100;
+double milesLeft = remainingFuel*mpg;
+
+
+
+// TODO: Output the results
+cout <<" The vechile can be driven " << milesLeft << " On the remaining fuel. " << endl;
+
 }
